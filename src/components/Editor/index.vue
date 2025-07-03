d33882cc5f5c2bfb04f8c1deefd35827e948f478
T34-active <a591342974@gmail.com>
2025-07-03 14:07:03 +0800

    feat: 更新包版本

--- src/components/Editor/index.vue
+++ src/components/Editor/index.vue
@@ -1,159 +1,147 @@
-<template>
-  <div class="editor">
-    <quill-editor
-      v-model:content="content"
-      contentType="html"
-      @textChange="(e) => $emit('update:modelValue', content)"
-      :options="options"
-      :style="styles"
-    />
-  </div>
-</template>
+<script setup lang="ts">
+import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
+import { getToken } from '@/utils/auth'
 
-<script lang="ts" setup>
-import { QuillEditor } from '@vueup/vue-quill'
-import '@vueup/vue-quill/dist/vue-quill.snow.css'
-import { computed, ref, watch } from 'vue'
-const props = defineProps({
-  /* 编辑器的内容 */
-  modelValue: {
-    type: String,
-  },
-  /* 高度 */
-  height: {
-    type: Number,
-    default: null,
-  },
-  /* 最小高度 */
-  minHeight: {
-    type: Number,
-    default: null,
-  },
-  /* 只读 */
-  readOnly: {
-    type: Boolean,
-    default: false,
-  },
-})
-const options = ref({
-  theme: 'snow',
-  bounds: document.body,
-  debug: 'warn',
-  modules: {
-    // 工具栏配置
-    toolbar: [
-      ['bold', 'italic', 'underline', 'strike'], // 加粗 斜体 下划线 删除线
-      ['blockquote', 'code-block'], // 引用  代码块
-      [{ list: 'ordered' }, { list: 'bullet' }], // 有序、无序列表
-      [{ indent: '-1' }, { indent: '+1' }], // 缩进
-      [{ size: ['small', false, 'large', 'huge'] }], // 字体大小
-      [{ header: [1, 2, 3, 4, 5, 6, false] }], // 标题
-      [{ color: [] }, { background: [] }], // 字体颜色、字体背景颜色
-      [{ align: [] }], // 对齐方式
-      ['clean'], // 清除文本格式
-      ['link', 'image', 'video'], // 链接、图片、视频
-    ],
-  },
-  placeholder: '请输入内容',
-  readOnly: props.readOnly,
-  // theme: 'snow',
-})
-const styles = computed(() => {
-  const style: any = {}
-  if (props.minHeight) {
-    style.minHeight = `${props.minHeight}px`
-  }
-  if (props.height) {
-    style.height = `${props.height}px`
-  }
-  return style
-})
+const { proxy } = getCurrentInstance()
+
+const emit = defineEmits(['update:modelValue']) // 触发事件，通知父组件
+
+const uploadUrl = ref(import.meta.env.VITE_APP_BASE_URL + '/website/upload/uploadImages')
+const headers = ref({ Authorization: 'Bearer ' + getToken() })
+const editorRef = shallowRef()
 const content = ref('')
+
+const props = defineProps({
+  modelValue: { type: String, default: '' }, // 绑定 v-model
+  height: { type: Number, default: null },
+  minHeight: { type: Number, default: null },
+  readOnly: { type: Boolean, default: false },
+  fileSize: { type: Number, default: 5 },
+  type: { type: String, default: 'url' },
+})
+
 watch(
   () => props.modelValue,
-  (v) => {
-    if (v !== content.value) {
-      content.value = v === undefined ? '<p></p>' : v
+  (newValue) => {
+    if (newValue !== content.value) {
+      content.value = newValue || '' // 同步 modelValue 到 content
     }
   },
   { immediate: true },
 )
+
+// 监听 content 变化，通知父组件
+watch(content, (newValue) => {
+  emit('update:modelValue', newValue)
+})
+
+// 编辑器配置
+const editorConfig = {
+  placeholder: '',
+  MENU_CONF: {
+    uploadImage: {
+      server: uploadUrl.value,
+      timeout: 5 * 1000, // 5s
+      meta: {
+        fileName: 'imageName',
+      },
+      fieldName: 'file',
+      metaWithUrl: false, // join params to url
+      headers: headers.value,
+      // maxFileSize: props.fileSize * 10,
+      maxFileSize: 10 * 1024 * 1024, // 10M
+      base64LimitSize: 5 * 1024, // insert base64 format, if file's size less than 5kb
+      onBeforeUpload(file) {
+        // console.log('onBeforeUpload', file);
+        // 获取动态键名
+        const fileKey = Object.keys(file)[0] // 获取第一个键名
+        const fileData = file[fileKey] // 访问该键名对应的值
+        // 访问文件的属性
+        const fileName = fileData.name // 获取文件名
+        const fileType = fileData.meta.type // 获取文件类型
+        const fileSize = fileData.size // 获取文件大小
+        const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/svg']
+        const isAllowedType = allowedTypes.includes(fileType)
+        // 检验文件格式
+        if (!isAllowedType) {
+          proxy.$modal.msgError('图片格式错误!')
+          return false // 阻止上传
+        }
+        // 校检文件大小
+        if (props.fileSize) {
+          const isLt = fileSize / 1024 / 1024 < props.fileSize
+          if (!isLt) {
+            proxy.$modal.msgError(`上传文件大小不能超过 ${props.fileSize} MB!`)
+            return false // 阻止上传
+          }
+        }
+        // console.log(
+        //   准备上传文件: ${fileName}, 类型: ${fileType}, 大小: ${fileSize} bytes
+        // );
+        return file // 允许上传
+      },
+      // 自定义插入图片
+      customInsert(res, insertFn) {
+        // console.log('服务器返回的图片数据：', res);
+        // 假设 res 中有 url 字段是图片的路径
+        if (res.code === 200 && res.msg) {
+          const url = res.msg // 获取图片 URL
+          const alt = res.msg || '' // 可以用原始文件名作为 alt
+          const href = '' // 可以用图片的链接作为 href，或者留空
+          insertFn(url, alt, href) // 调用插入函数
+          proxy.$modal.msgSuccess('上传成功')
+        } else {
+          // console.error('图片上传失败：' + (res.msg || '未知错误'));
+        }
+      },
+      onProgress(progress) {
+        // console.log('onProgress', progress);
+      },
+      onSuccess(file, res) {
+        // console.log('onSuccess', file, res);
+      },
+      onFailed(file, res) {
+        // console.error('onFailed', file, res);
+      },
+      onError(file, err, res) {
+        // console.error('onError', file, err, res);
+      },
+    },
+  },
+}
+
+const toolbarConfig = { excludeKeys: ['fullScreen', 'group-video', 'emotion'] }
+
+const handleCreated = (editor) => {
+  editorRef.value = editor
+}
+
+onBeforeUnmount(() => {
+  editorRef.value?.destroy()
+})
 </script>
 
-<style>
-.editor,
-.ql-toolbar {
-  white-space: pre-wrap !important;
-  line-height: normal !important;
-}
-.quill-img {
-  display: none;
-}
-.ql-snow .ql-tooltip[data-mode='link']::before {
-  content: '请输入链接地址:';
-}
-.ql-snow .ql-tooltip.ql-editing a.ql-action::after {
-  border-right: 0px;
-  content: '保存';
-  padding-right: 0px;
-}
-.ql-snow .ql-tooltip[data-mode='video']::before {
-  content: '请输入视频地址:';
-}
-.ql-snow .ql-picker.ql-size .ql-picker-label::before,
-.ql-snow .ql-picker.ql-size .ql-picker-item::before {
-  content: '14px';
-}
-.ql-snow .ql-picker.ql-size .ql-picker-label[data-value='small']::before,
-.ql-snow .ql-picker.ql-size .ql-picker-item[data-value='small']::before {
-  content: '10px';
-}
-.ql-snow .ql-picker.ql-size .ql-picker-label[data-value='large']::before,
-.ql-snow .ql-picker.ql-size .ql-picker-item[data-value='large']::before {
-  content: '18px';
-}
-.ql-snow .ql-picker.ql-size .ql-picker-label[data-value='huge']::before,
-.ql-snow .ql-picker.ql-size .ql-picker-item[data-value='huge']::before {
-  content: '32px';
-}
-.ql-snow .ql-picker.ql-header .ql-picker-label::before,
-.ql-snow .ql-picker.ql-header .ql-picker-item::before {
-  content: '文本';
-}
-.ql-snow .ql-picker.ql-header .ql-picker-label[data-value='1']::before,
-.ql-snow .ql-picker.ql-header .ql-picker-item[data-value='1']::before {
-  content: '标题1';
-}
-.ql-snow .ql-picker.ql-header .ql-picker-label[data-value='2']::before,
-.ql-snow .ql-picker.ql-header .ql-picker-item[data-value='2']::before {
-  content: '标题2';
-}
-.ql-snow .ql-picker.ql-header .ql-picker-label[data-value='3']::before,
-.ql-snow .ql-picker.ql-header .ql-picker-item[data-value='3']::before {
-  content: '标题3';
-}
-.ql-snow .ql-picker.ql-header .ql-picker-label[data-value='4']::before,
-.ql-snow .ql-picker.ql-header .ql-picker-item[data-value='4']::before {
-  content: '标题4';
-}
-.ql-snow .ql-picker.ql-header .ql-picker-label[data-value='5']::before,
-.ql-snow .ql-picker.ql-header .ql-picker-item[data-value='5']::before {
-  content: '标题5';
-}
-.ql-snow .ql-picker.ql-header .ql-picker-label[data-value='6']::before,
-.ql-snow .ql-picker.ql-header .ql-picker-item[data-value='6']::before {
-  content: '标题6';
-}
-.ql-snow .ql-picker.ql-font .ql-picker-label::before,
-.ql-snow .ql-picker.ql-font .ql-picker-item::before {
-  content: '标准字体';
-}
-.ql-snow .ql-picker.ql-font .ql-picker-label[data-value='serif']::before,
-.ql-snow .ql-picker.ql-font .ql-picker-item[data-value='serif']::before {
-  content: '衬线字体';
-}
-.ql-snow .ql-picker.ql-font .ql-picker-label[data-value='monospace']::before,
-.ql-snow .ql-picker.ql-font .ql-picker-item[data-value='monospace']::before {
-  content: '等宽字体';
+<template>
+  <div class="border">
+    <Toolbar
+      :editor="editorRef"
+      style="border-bottom: 1px solid #ccc"
+      :defaultConfig="toolbarConfig"
+    />
+    <Editor
+      v-model="content"
+      :defaultConfig="editorConfig"
+      style="height: 600px; overflow-y: hidden"
+      @onCreated="handleCreated"
+    />
+  </div>
+</template>
+
+<style src="@wangeditor/editor/dist/css/style.css"></style>
+<style scoped>
+.border {
+  border: 1px solid #ccc;
+  border-radius: 1px;
+  width: 100%;
 }
 </style>
